59efe2e535fd3a6173381c9baeacda86f792b631
Joris Portegies Zwart <joris.portegies.zwart@ximedes.com>
2020-12-07 12:00:57 +0100

    Day 7

--- src/commonMain/kotlin/Runner.kt
+++ src/commonMain/kotlin/Runner.kt
@@ -2,13 +2,14 @@ import day02.p02
 import day04.p04
 import day05.p05
 import day06.p06
+import day07.p07
 import day1.p01
 import kotlin.math.sqrt
 
 
 suspend fun run(repeat: Int, day: Int, platform: String) {
     println("Running day $day $repeat times on platform $platform")
-    val puzzles = listOf(p01, p02, p03, p04, p05, p06)
+    val puzzles = listOf(p01, p02, p03, p04, p05, p06, p07)
     val times = (1..repeat).map {
         measureNanos {
             val p = puzzles[day - 1]
